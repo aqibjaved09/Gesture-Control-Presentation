1cbf3f986d525c3cea2a4e93b127e709d2ac7328
Muhammad Aqib javeed <maqibjaveed4@gmail.com>
2025-08-01 18:54:07 +0500

    Use Helper function Thumb Line Movment, Fingger vertical Movment

--- ppt_gesture.py
+++ ppt_gesture.py
@@ -22,3 +22,26 @@ gesture_cooldown = 1.2  # seconds
 mp_hands = mp.solutions.hands
 hands_model = mp_hands.Hands(max_num_hands=1)
 drawer = mp.solutions.drawing_utils
+
+def get_finger_status(hand_landmarks):
+    """
+    Determines which fingers are up based on landmark positions.
+    Returns a list of 0s and 1s (1 for finger up).
+    """
+    tips = [4, 8, 12, 16, 20]
+    finger_states = []
+
+    # Thumb (check horizontal movement)
+    if hand_landmarks.landmark[tips[0]].x < hand_landmarks.landmark[tips[0] - 1].x:
+        finger_states.append(1)
+    else:
+        finger_states.append(0)
+
+    # Other fingers (check vertical movement)
+    for i in range(1, 5):
+        if hand_landmarks.landmark[tips[i]].y < hand_landmarks.landmark[tips[i] - 2].y:
+            finger_states.append(1)
+        else:
+            finger_states.append(0)
+
+    return finger_states
